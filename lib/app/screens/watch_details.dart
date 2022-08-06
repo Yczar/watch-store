9e9944d4421da53fff0e4a5ad4931c956a23c1e8
Yczar <babalolagbogo@gmail.com>
2022-08-06 11:21:18 +0100

    created a awatch details screen file

---
+++ lib/app/screens/watch_details.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class WatchDetailsScreen extends StatefulWidget {
+  const WatchDetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WatchDetailsScreen> createState() => _WatchDetailsScreenState();
+}
+
+class _WatchDetailsScreenState extends State<WatchDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
