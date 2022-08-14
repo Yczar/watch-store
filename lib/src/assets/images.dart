f1e5867d527806817772c09518b5410d3239d9c0
Yczar <babalolagbogo@gmail.com>
2022-08-14 09:53:25 +0100

    updated the images.dart file

---
+++ lib/src/assets/images.dart
@@ -0,0 +1,9 @@
+const String _imgDir = 'assets/images';
+const String _watchImgDir = '$_imgDir/watches';
+
+/// [Watches]
+///
+const String kAllSeriesWatchImg = '$_watchImgDir/all_series.png';
+const String kProSeriesWatchImg = '$_watchImgDir/pro_series.png';
+const String kSeriesWatch4Img = '$_watchImgDir/series_4.png';
+const String kSeriesWatch7 = '$_watchImgDir/series_7.png';
