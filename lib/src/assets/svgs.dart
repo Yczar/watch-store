fb9b89ee28a729f2246cddb09c4c362623d8d526
Yczar <babalolagbogo@gmail.com>
2022-08-14 09:53:35 +0100

    updated the svgs.dart file

---
+++ lib/src/assets/svgs.dart
@@ -0,0 +1,13 @@
+const String _svgDir = 'assets/svgs';
+const String _homeSvgDir = '$_svgDir/home';
+const String _watchesSvgDir = '$_svgDir/watches';
+
+/// [Home]
+///
+const String kHomeMenuSvg = '$_homeSvgDir/menu.svg';
+const String kHomeSearchSvg = '$_homeSvgDir/search.svg';
+
+/// [Watches]
+///
+const String kWatchesLikeSvg = '$_watchesSvgDir/like.svg';
+const String kWatchesBackArrowSvg = '$_watchesSvgDir/back_arrow.svg';
